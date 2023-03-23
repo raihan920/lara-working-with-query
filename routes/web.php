2d22be1dfa5ebeae83d121dc54c59d8117d335ac
Raihan Chowdhury <raihan92@rocketmail.com>
2023-03-23 15:23:58 +0600

    test query run

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/query', function(){
+    $results = DB::table('customers')->get();
+    echo "<pre>";
+    print_r($results);
+    echo "</pre>";
 });
