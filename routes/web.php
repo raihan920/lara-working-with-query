88eb4bec6ae59e25540a309e3f45b7ef9388f1a1
Raihan Chowdhury <raihan92@rocketmail.com>
2023-03-28 23:27:26 +0600

    working with orders and orderDetails tables

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,26 @@ Route::get('/query', function(){
     // echo "</pre>";
 
 
+    // $results = DB::table('products')
+    //         ->join('productlines', 'products.productLine', '=', 'productlines.productLine')
+    //         ->select('products.*', 'productlines.textDescription')
+    //         ->get();
+    // echo "<pre>";
+    // print_r($results);
+    // echo "</pre>";
 
-    $results = DB::table('products')
-    ->join('productlines', 'products.productLine', '=', 'productlines.productLine')
-    ->select('products.*', 'productlines.textDescription')
-    ->get();
+    // $results = DB::table('products as t1')
+    //             ->join('productlines as t2', 't1.productLine', '=', 't2.productLine')
+    //             ->select('productCode', 'productName', 'textDescription')
+    //             ->get();
+    // echo "<pre>";
+    // print_r($results);
+    // echo "</pre>";
+
+    $results = DB::table('orders')
+                ->join('orderdetails', 'orders.orderNumber', '=', 'orderdetails.orderNumber')
+                ->select('orders.orderNumber', 'orders.status', DB::raw('SUM(quantityOrdered*priceEach) as total'))
+                ->get();
     echo "<pre>";
     print_r($results);
     echo "</pre>";
